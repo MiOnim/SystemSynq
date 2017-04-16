a507194aae6fdcbc3134bc0fe21c4385183a478b
MiOnim <monim1@pride.hofstra.edu>
2017-04-16 18:42:11 -0400

    running cpu_usage in a different thread

--- AgentSoftware/agent.py
+++ AgentSoftware/agent.py
@@ -6,7 +6,10 @@ Author: Mazharul Onim
 
 import multiprocessing
 import wmi
+import time
+from Queue import Queue
 from datetime import datetime
+from threading import Thread
 from wmic import Wmic
 
 wmi_obj = wmi.WMI()
@@ -48,9 +51,40 @@ def pretty_print_time(value):
     
     return dt.strftime("%Y/%m/%d %H:%M:%S")
 
+def enthread(target):
+    q = Queue()
+    def wrapper():
+        q.put(target())
+    t = Thread(target=wrapper)
+    t.start()
+    return q    
+
+def threaded_cpu_usage():
+    cpu_usage = Wmic("cpu", "LoadPercentage").run().get_value()
+    print "CPU Usage:", cpu_usage
+
+def threaded_time_last_shutdown():
+    import pythoncom
+    pythoncom.CoInitialize()
+    w = wmi.WMI()
+    try:
+        wql = "SELECT * FROM Win32_NTLogEvent WHERE LogFile='System' AND EventCode='6006'"
+        wql_run = w.query(wql)
+        last_shutdown = wql_run[0].TimeGenerated
+        print "Last shutdown: ", last_shutdown
+    finally:
+        pythoncom.CoUninitialize()
+    
+
+start = time.time()    
+
 num_cpu = multiprocessing.cpu_count()
 clock_speed = Wmic("cpu", "MaxClockSpeed").run().get_value()
-cpu_usage = Wmic("cpu", "LoadPercentage").run().get_value()
+#cpu = enthread(target=Wmic("cpu", "LoadPercentage").run().get_value())
+#cpu_usage = cpu.get()
+cpu_usage=""
+thread = Thread(target=threaded_cpu_usage)
+thread.start()
 ram_total = Wmic("computersystem", "TotalPhysicalMemory").run().get_gigabytes()
 ram_free = Wmic("os", "FreePhysicalMemory").run().get_gigabytes()
 disk_total = get_total_disk_space()
@@ -59,11 +93,14 @@ disk_free = get_free_disk_space()
 num_process = Wmic("os", "NumberOfProcesses").run().get_value()
 last_bootup = pretty_print_time(Wmic("os", "LastBootupTime").run().get_value())
 last_shutdown = pretty_print_time(time_last_shutdown())
+#thread2 = Thread(target=threaded_time_last_shutdown)
+#thread2.start()
+#thread2.join()
 
 
 print "Number of CPU:", num_cpu
 print "Clock Speed:", clock_speed
-print "CPU Usage:", cpu_usage
+#print "CPU Usage:", cpu_usage
 print "Total RAM:", ram_total
 print "Free RAM:", ram_free
 print "Total Disk Space:", disk_total
@@ -72,3 +109,6 @@ print "Free Disk Space:", disk_free
 print "Number of Processes:", num_process
 print "Last bootup: ", last_bootup
 print "Last shutdown: ", last_shutdown
+#thread2.join()
+end = time.time()
+print (end-start)
