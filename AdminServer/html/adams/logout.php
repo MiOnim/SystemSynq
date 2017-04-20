8567bbad9766ddf95b172e5f9ace0f0f102a24e5
mgavin4 <mgavin4@pride.hofstra.edu>
2017-04-20 19:18:35 -0400

    dump and removed unused files

--- AdminServer/html/adams/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-  session_start();
-  extract($_COOKIE);
-  setcookie("sessionid",$sessionid,time()-3600);
-  var_dump($_COOKIE);
-  header("location:./login2.html");
-  die;
-?>
--- AdminServer/html/dump.sql
+++ AdminServer/html/dump.sql
@@ -16,14 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `systemsynq`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `systemsynq` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `systemsynq`;
-
---
 -- Table structure for table `HardwareSoftware`
 --
 
@@ -44,14 +36,40 @@ CREATE TABLE `HardwareSoftware` (
 --
 -- Dumping data for table `HardwareSoftware`
 --
+
+LOCK TABLES `HardwareSoftware` WRITE;
+/*!40000 ALTER TABLE `HardwareSoftware` DISABLE KEYS */;
+INSERT INTO `HardwareSoftware` VALUES ('1','Adams-203001','20%','4096MB','235520MB','5','malware.exe,badprocesses.exe'),('2','Adams-203002','35%','1024MB','234410MB','10','virus.exe,malicious.exe,ransomware.exe');
+/*!40000 ALTER TABLE `HardwareSoftware` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `MachineInfo`
+--
+
+DROP TABLE IF EXISTS `MachineInfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `MachineInfo` (
+  `ID` varchar(255) DEFAULT NULL,
   `Name` varchar(255) DEFAULT NULL,
   `CPU_Cores` varchar(255) DEFAULT NULL,
   `Clock_Speed` varchar(255) DEFAULT NULL,
   `RAM_Total` varchar(255) DEFAULT NULL,
   `DISK_Total` varchar(255) DEFAULT NULL,
+  `On_Off_Flag` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `MachineInfo`
+--
+
+LOCK TABLES `MachineInfo` WRITE;
+/*!40000 ALTER TABLE `MachineInfo` DISABLE KEYS */;
+INSERT INTO `MachineInfo` VALUES ('1','Adams-203001','4','2.3GHz','8096MB','1048576MB','On');
+/*!40000 ALTER TABLE `MachineInfo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Notifications`
@@ -96,7 +114,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('1095239199','admin'),('1556387894','admin'),('673517808','admin'),('937939447','admin'),('425547398','admin'),('1151843643','admin'),('613802138','admin'),('504071283','admin'),('774892333','admin'),('333347737','admin'),('1976641442','admin'),('315860743','admin'),('466621425','hello'),('1521487634','another test'),('646353394','admin'),('2101208410','admin'),('1906089579','admin'),('1778586434','admin'),('2013379738','admin'),('1777217575','admin'),('705795292','admin'),('1416427282','admin'),('1786905478','admin'),('408795248','admin'),('756185972','admin'),('987467062','test'),('1673412384','admin'),('666043228','admin'),('2037398695','admin'),('269647326','admin'),('889286584','admin'),('517831161','admin'),('1445152892','admin'),('1438309548','admin'),('460596229','admin'),('1976850414','admin'),('2001018801','admin'),('1036661173','admin'),('1821380217','admin'),('1707221856','admin'),('1184349155','admin'),('1615760213','admin'),('334726198','admin'),('1615483204','admin'),('728495583','admin'),('1354030369','admin'),('816099471','admin'),('1433227361','admin'),('552913185','admin'),('249508022','admin'),('1418861983','admin'),('511356034','admin'),('1747282487','admin'),('1925042876','admin'),('1471118429','admin'),('1528428051','admin'),('481837254','admin');
+INSERT INTO `sessions` VALUES ('1095239199','admin'),('1556387894','admin'),('673517808','admin'),('937939447','admin'),('425547398','admin'),('1151843643','admin'),('613802138','admin'),('504071283','admin'),('774892333','admin'),('333347737','admin'),('1976641442','admin'),('315860743','admin'),('466621425','hello'),('1521487634','another test'),('646353394','admin'),('2101208410','admin'),('1906089579','admin'),('1778586434','admin'),('2013379738','admin'),('1777217575','admin'),('705795292','admin'),('1416427282','admin'),('1786905478','admin'),('408795248','admin'),('756185972','admin'),('987467062','test'),('1673412384','admin'),('666043228','admin'),('2037398695','admin'),('269647326','admin'),('889286584','admin'),('517831161','admin'),('1445152892','admin'),('1438309548','admin'),('460596229','admin'),('1976850414','admin'),('2001018801','admin'),('1036661173','admin'),('1821380217','admin'),('1707221856','admin'),('1184349155','admin'),('1615760213','admin'),('334726198','admin'),('1615483204','admin'),('728495583','admin'),('1354030369','admin'),('816099471','admin'),('1433227361','admin'),('552913185','admin'),('249508022','admin'),('1418861983','admin'),('511356034','admin'),('1747282487','admin'),('1925042876','admin'),('1471118429','admin'),('1528428051','admin'),('481837254','admin'),('1318570179','admin'),('2071692703','matt'),('78484239','bob'),('1308546996','matt'),('985456868','matt'),('790712811','matt'),('1483778219','matt'),('1412970461','matt'),('8637878','matt');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +138,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('admin','admin','all'),('hello','hi there','hello'),('test','$2y$10$kFV6z4PcwYgrqW6FgW2sk.cWjzcfEp4q6YpbI0TZBaenq/7flY6PG','test'),('another test','$2y$10$arwkPd0XIzZODROlTKrhOuNesn2uVYiNLFSOeqYguP/IlHMW35LBy','another test'),('one more test','$2y$10$anficB4eIf68asiaYsxkpu7sSQ6XcOs8buOgV.3FzsdyMQYP4g5xy','one more test');
+INSERT INTO `users` VALUES ('admin','admin','all'),('hello','hi there','hello'),('test','$2y$10$kFV6z4PcwYgrqW6FgW2sk.cWjzcfEp4q6YpbI0TZBaenq/7flY6PG','test'),('another test','$2y$10$arwkPd0XIzZODROlTKrhOuNesn2uVYiNLFSOeqYguP/IlHMW35LBy','another test'),('one more test','$2y$10$anficB4eIf68asiaYsxkpu7sSQ6XcOs8buOgV.3FzsdyMQYP4g5xy','one more test'),('matt','$2y$10$Cn153R4U9QBD3mWdVzBH2O8tWTiJeGFqstxg6oajBCMfvCbH9IUKW','matt'),('bob','$2y$10$aASEz5kOJQ.Vv2rVu3HymehTe6FEyzR4EsVJlzmtfoHp7iTACC4rG','bob');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -133,4 +151,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-19 22:42:31
+-- Dump completed on 2017-04-20 18:38:10
