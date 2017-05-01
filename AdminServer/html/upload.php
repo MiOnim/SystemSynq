1eb035709ba8022e30ed348a77122285d1118863
MiOnim <monim1@pride.hofstra.edu>
2017-05-01 01:18:27 -0400

    updated upload.php to send json response and appropriate http response code. Added an htaccess file

--- AdminServer/html/upload.php
+++ AdminServer/html/upload.php
@@ -6,18 +6,19 @@
  */
  
 $uploaddir = '/var/www/event-uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+$uploadfile = $uploaddir.basename($_FILES['userfile']['name']);
 
-echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "Possible file upload attack!\n";
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
+{
+    $json = array("success" => "1");
+    echo json_encode($json);
+    http_response_code(200);
+}
+else {
+    $json = array("error" => "file upload failed");
+    echo json_encode($json);
+    http_response_code(409);
 }
 
-echo 'Here is some more debugging info:';
-print_r($_FILES);
-
-print "</pre>";
-
 ?>
+
